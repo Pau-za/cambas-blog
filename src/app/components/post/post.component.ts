5765e1e2e55b0185c38759aef943b4bfc1c97bdd
Pau-za <molo_pau@hotmail.com>
2019-07-10 15:14:00 -0500

    gets comments of the posts and prints it on the post component

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
 import { BlogDataService } from "../../services/blog-data.service";
 import { LoginService } from "../../services/login.service";
+import { PostService } from "../../services/post.service";
 
 @Component({
   selector: 'app-post',
@@ -12,34 +13,39 @@ export class PostComponent {
 
   post: any;
   postAuthor: any;
+  commentsPost: any[] = [];
 
   constructor(private activatedRoute: ActivatedRoute,
     private blogService: BlogDataService,
-    private loginService: LoginService) {
+    private loginService: LoginService,
+    private postService: PostService) {
     this.activatedRoute.params.subscribe(params => {
-      // console.log(params['id']);
       this.post = this.blogService.getPosts().subscribe((res: any) => {
         res.map((element) => {
           if (element.title === (params['id'])) {
-            console.log(element);
             return this.post = element;
           }
         })
-        // this.post = res.;
-        // console.log(this.tenPosts);
       })
     })
 
-    this.loginService.getUsers().subscribe((res:any) => {
+    this.loginService.getUsers().subscribe((res: any) => {
       res.map((element) => {
-        if(element.id === this.post.author){
+        if (element.id === this.post.author) {
           this.postAuthor = element;
-          console.log(this.postAuthor);
         }
       })
-      
+    })
+
+    this.postService.getComentsPost().subscribe((res: any) => {
+      res.map((element) => {
+        if (element.postId === this.post.author) {
+          this.commentsPost.push(element);
+        }
+      })
     })
   }
+
 }
 
 
